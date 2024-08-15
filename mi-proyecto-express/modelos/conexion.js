89f46a8160f0a8e0e370276b4a1a3c81689bdf54
wilsondevilla <wilsondevilla123@gmail.com>
2024-08-15 17:55:41 -0400

    conexcion establecida

--- mi-proyecto-express/modelos/conexion.js
+++ mi-proyecto-express/modelos/conexion.js
@@ -1,14 +1,29 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('postgres://postgres:1234@localhost:5432/wil') // Example for postgres
 
-function probarconnexion(){
-try {
-     sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
+
+
+const User =  sequelize.define("user",{
+  nombre:{
+    type:DataTypes.STRING,
+    allowNull:false
+  },
+  contraseña:{
+    type:DataTypes.STRING,
+    allowNull:false
   }
 
-}
+})
 
-module.exports=probarconnexion
+async function probarconnexion(){
+  try {
+    await sequelize.sync({force:true})
+       sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+    
+  
+  }
+  module.exports=probarconnexion
